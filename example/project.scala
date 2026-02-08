65d18e36fdfc368a4742803772f277d23acea0da
Joachim Bargsten <jw@bargsten.org>
2026-02-08 20:40:19 +0100

    make it thread safe

--- example/project.scala
+++ example/project.scala
@@ -1,9 +1,9 @@
 //> using scala 3.8.1
 // :snx dependencies
 // Scala CLI
-//> using dep org.bargsten::valacc:0.0.1
+//> using dep org.bargsten::valacc:0.0.2
 // sbt
-// "org.bargsten" %% "valacc" % "0.0.1"
+// "org.bargsten" %% "valacc" % "0.0.2"
 // :xns
 //> using repository "ivy2Local"
 //> using repository "m2Local"
--- src/main/scala/org/bargsten/valacc/ValidationScope.scala
+++ src/main/scala/org/bargsten/valacc/ValidationScope.scala
@@ -2,15 +2,17 @@ package org.bargsten.valacc
 
 import cats.data.NonEmptyList
 import scala.collection.mutable.ListBuffer
+import java.util.concurrent.ConcurrentLinkedQueue
+import scala.jdk.CollectionConverters.*
 
 private[valacc] class ValidationException extends Exception with scala.util.control.NoStackTrace
 
 class ValidationScope[E]:
-  private val _errors = ListBuffer.empty[E]
+  private val _errors = ConcurrentLinkedQueue[E]()
   private val abort = new ValidationException
 
   private[valacc] def build[A](value: A): Validated[E, A] =
-    NonEmptyList.fromList(_errors.toList) match
+    NonEmptyList.fromList(_errors.asScala.toList) match
       case Some(nel) => Invalid(nel)
       case None      => Valid(value)
 
@@ -20,75 +22,76 @@ class ValidationScope[E]:
   // --- accumulating ---
 
   def ensure(predicate: Boolean)(error: => E): Unit =
-    if !predicate then _errors += error
+    if !predicate then _errors.add(error)
 
   def ensureFail(error: => E): Unit =
-    _errors += error
+    _errors.add(error)
 
   def ensureDefined[A](option: Option[A])(error: => E): Option[A] =
     option match
       case some @ Some(_) => some
-      case None           => _errors += error; None
+      case None           => _errors.add(error); None
 
   def ensureValue[A](validated: Validated[E, A]): Option[A] =
     validated match
       case Valid(a)        => Some(a)
-      case Invalid(errors) => _errors ++= errors.toList; None
+      case Invalid(errors) => _errors.addAll(errors.toList.asJava); None
 
   def attach[A](validated: Validated[E, A]): Validated[E, A] =
     validated match
-      case inv @ Invalid(errors) => _errors ++= errors.toList; inv
+      case inv @ Invalid(errors) => _errors.addAll(errors.toList.asJava); inv
       case v                     => v
 
   // --- short-circuiting ---
 
   def demand(predicate: Boolean)(error: => E): Unit =
     if !predicate then
-      _errors += error
+      _errors.add(error)
       shortcircuit()
 
   def demandFail(error: => E): Nothing =
-    _errors += error
+    _errors.add(error)
     shortcircuit()
 
   def demandDefined[A](option: Option[A])(error: => E): A =
     option match
       case Some(a) => a
-      case None    => _errors += error; shortcircuit()
+      case None    => _errors.add(error); shortcircuit()
 
   def demandValue[A](validated: Validated[E, A]): A =
     validated match
       case Valid(a) => a
       case Invalid(errors) =>
-        _errors ++= errors.toList
+        _errors.addAll(errors.toList.asJava)
         shortcircuit()
 
   def demandValid(validated: Validated[E, ?]): Unit =
     validated match
       case Valid(_) => ()
       case Invalid(errors) =>
-        _errors ++= errors.toList
+        _errors.addAll(errors.toList.asJava)
         shortcircuit()
 
   // --- extensions available via given scope ---
 
   extension [A](validated: Validated[E, A])
     def get: A = validated match
-      case Valid(a)   => a
+      case Valid(a)      => a
       case _: Invalid[?] => shortcircuit()
 
     def attachV(): Validated[E, A] = attach(validated)
 
   extension [A](value: A)
     def ensure(predicate: A => Boolean)(error: => E): A =
-      if !predicate(value) then _errors += error
+      if !predicate(value) then _errors.add(error)
       value
 
     def demand(predicate: A => Boolean)(error: => E): A =
       if !predicate(value) then
-        _errors += error
+        _errors.add(error)
         shortcircuit()
       value
+end ValidationScope
 
 object ValidationScope:
   def validate[E](block: ValidationScope[E] ?=> Unit): Validated[E, Unit] =
@@ -108,4 +111,3 @@ object ValidationScope:
     catch
       case t: ValidationException if scope.isOurException(t) =>
         scope.build(()).asInstanceOf[Validated[E, A]]
-
